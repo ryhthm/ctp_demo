70d82ff8edc573f04a1c7bee3ad90846a643e6fa
杰克 李 <virlhs@163.com>
2021-07-25 15:34:18 +0800

    using https://github.com/tashaxing/CTPtest and https://blog.csdn.net/u012234115/article/details/70195889

---
+++ 6.3.15_demo/StrategyTrade.h
@@ -0,0 +1,22 @@
+#pragma once
+// ---- 简单策略交易的类 ---- //
+
+#include "stdafx.h"
+#include <functional>
+#include "ThostFtdcUserApiStruct.h"
+#include "TickToKlineHelper.h"
+#include "CustomTradeSpi.h"
+
+typedef void(*reqOrderInsertFun)(
+	TThostFtdcInstrumentIDType instrumentID,
+	TThostFtdcPriceType price,
+	TThostFtdcVolumeType volume,
+	TThostFtdcDirectionType direction);
+
+using ReqOrderInsertFunctionType = std::function<
+	void(TThostFtdcInstrumentIDType instrumentID,
+	TThostFtdcPriceType price,
+	TThostFtdcVolumeType volume,
+	TThostFtdcDirectionType direction)>;
+
+void StrategyCheckAndTrade(TThostFtdcInstrumentIDType instrumentID, CustomTradeSpi *customTradeSpi);
